4af0b62341bacca2a607fbf7dda18321d0384230
Muhammad Rouhan <rouhancyber123@gmail.com>
2025-12-01 00:21:11 +0500

    Add Flask app with prediction endpoint and home page template

---
+++ app.py
@@ -0,0 +1,26 @@
+import pickle
+from django.shortcuts import render
+from flask import Flask, request, jsonify, app, url_for, render_template
+import numpy as np
+import pandas as pd
+
+app = Flask(__name__)
+##Load the model
+pickle_model = pickle.load(open("delivery_cost_model.pkl", "rb"))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json('data')
+    print(data)
+    data_df = pd.DataFrame(data)
+    output = pickle_model.predict(data_df)
+    print(output[0])
+    return jsonify({'prediction': list(output[0])})
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
