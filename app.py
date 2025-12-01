6008b1d3c122419a1cc81a44cc5b8b8a0de008ac
Muhammad Rouhan <rouhancyber123@gmail.com>
2025-12-01 13:14:04 +0500

    Refactor prediction routes and enhance home page layout with improved styling and prediction display

--- app.py
+++ app.py
@@ -12,14 +12,23 @@ pickle_model = pickle.load(open("delivery_cost_model.pkl", "rb"))
 def home():
     return render_template('home.html')
 
-@app.route('/predict', methods=['POST'])
-def predict():
+@app.route('/predict_api', methods=['POST'])
+def predict_api():
     data = request.json('data')
     print(data)
     data_df = pd.DataFrame(data)
     output = pickle_model.predict(data_df)
     print(output[0])
     return jsonify({'prediction': list(output[0])})
+
+@app.route('/predict',methods = ['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = pd.DataFrame(np.array(data).reshape(1,-1))
+    print(final_input)
+    output = pickle_model.predict(final_input)[0]
+    print(output)
+    return render_template("home.html", prediction_text = "The predicted delivery cost is Rs. {}".format(output))
 
 if __name__ == "__main__":
     app.run(debug=True)
